4bb6b0ad2e440f38b6122061d3354cc100a45abf
deanbest <deanbest5320@gmail.com>
2022-09-17 16:01:10 +0800

    cal avg

--- new_read.py
+++ new_read.py
@@ -8,4 +8,12 @@ with open('test.txt', 'r') as f:
 			print(len(data))
 		
 
-print(data[1])
+print('檔案讀完, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('平均每筆資料長度為', sum_len / len(data))
+
